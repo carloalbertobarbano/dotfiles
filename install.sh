af2fd2b2a08e2f0c7c825dbc7ce5a579b44230fc
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-04-02 00:33:09 +0200

    install rofi

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 
 #!/bin/sh
 
+yay -S rofi
+
 mkdir ~/.config/i3status
-
-
 mv ~/.config/i3/config ~/.config/i3/confing_bak 
 ln -s $(pwd)/i3/config ~/.config/i3/config
-ln -s $(pwd)/i3status/config ~/.config/i3/config
+ln -s $(pwd)/i3status/config ~/.config/i3status/config
+cp $(pwd)/i3status/ram_i3status.sh ~/.config/i3status/ram_i3status.sh
+
+
