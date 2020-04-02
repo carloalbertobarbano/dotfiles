992290d52c264f259574cf136a099e987a74bb92
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-04-02 11:58:47 +0200

    update install

--- install.sh
+++ install.sh
@@ -9,5 +9,7 @@ mv ~/.config/i3/config ~/.config/i3/confing_bak
 ln -s $(pwd)/i3/config ~/.config/i3/config
 ln -s $(pwd)/i3status/config ~/.config/i3status/config
 cp $(pwd)/i3status/ram_i3status.sh ~/.config/i3status/ram_i3status.sh
-
+cp Xresources ~/.Xresources
+mkdir ~/.config/rofi
+cp rofi/config ~/.config/rofi/config
 
